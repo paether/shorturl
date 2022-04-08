3d1de3e1c918427127e9c7e2d693e54bd3836722
paether <petersocial@icloud.com>
2022-04-08 11:46:08 +0200

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="main-container">
       <Main />
+      <Footer />
     </div>
   );
 }
